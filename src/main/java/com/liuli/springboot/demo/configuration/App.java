e3428dc779d8ca817e8d9b1d22e54018661a84d9
刘利 <li.liu@jrj.com.cn>
2017-09-11 18:26:55 +0800

    初始化

---
+++ src/main/java/com/liuli/springboot/demo/configuration/App.java
@@ -0,0 +1,20 @@
+package com.liuli.springboot.demo.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * Spring Boot 启动类
+ * @author li.liu
+ */
+@SpringBootApplication
+@EnableAsync
+@ComponentScan(basePackages = "com.liuli")// 开启配置属性支持
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/liuli/springboot/demo/web/controller/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.liuli.springboot.demo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Hello World
+ * @author li.liu
+ */
+
+@Controller
+@RequestMapping("/demo")
+public class HelloWorld {
+
+    @RequestMapping(value = "/helloWorld/{name}")
+    public String helloWorld(@PathVariable("name") String name,
+                             Model model) {
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/helloWorldApi/{name}")
+    @ResponseBody
+    public String helloWorldApi(@PathVariable("name") String name) {
+        return "hello " + name;
+    }
+}
